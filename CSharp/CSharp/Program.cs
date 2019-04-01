a61217a1067545b76c54b5d7a036fb856afa3353
Boris <fody@protonmail.com>
2019-04-01 11:23:09 +0300

    update

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -6,21 +6,10 @@ using System.Threading.Tasks;
 
 namespace CSharp
 {
-    class MyClass {
-        public void a() {
-         Console.WriteLine("Hi");
-}
-        public int sum(int a, int b) {
-            return a + b;
-}
-}
     class Program
     {
     static void Main(string[] args)
 {
-    MyClass a = new MyClass();
-    a.a();
-    Console.WriteLine(a.sum(1,3));
     Console.ReadKey();
 }
 }
