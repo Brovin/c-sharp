6817fd09539f6a401f0e8b7087b0a8d3a72f92d4
Boris <fody@protonmail.com>
2019-04-08 19:11:26 +0300

    update

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -8,10 +8,20 @@ namespace CSharp
 {
     class Program
     {
-    static void Main(string[] args)
-{
-    Console.WriteLine("Hi");
-    Console.Read();
+        static void Main(string[] args)
+        {
+            int[] a = {1,5,32313,125,124,23,3};
+            Console.WriteLine(maxElement(a));
+            Console.ReadKey();
+        }
+
+        public static int maxElement(int[] numbers) {
+            int max = numbers[0];
+            foreach(int el in numbers) {
+                if(el > max)
+                    max = el;
 }
+            return max;
 }
+    }
 }
