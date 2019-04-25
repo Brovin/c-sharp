d85a36489a5e29d65cb52b8da58f5d856c73a27e
Boris <fody@protonmail.com>
2019-04-25 13:35:00 +0300

    Learned StreamWriter and StreamReader

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace CSharp
 {
@@ -10,7 +11,19 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            
+            string file_name = @"C:\Users\Spark\Desktop\c-sharp\CSharp\CSharp\text.txt";
+            string text = "Hello man";
+
+            StreamWriter sw = File.CreateText(file_name);
+            sw.WriteLine(text);
+            sw.WriteLine("How are you ?");
+            sw.Close();
+
+            StreamReader sr = File.OpenText(file_name);
+            Console.WriteLine(sr.ReadToEnd());
+            sr.Close();
+
+            Console.ReadKey();
         }
     }
 }
