03d24f877577960e8a7c1c6d9d4104e5ae0b21fe
Boris <fody@protonmail.com>
2019-04-26 18:32:14 +0300

    Learned constructor

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -9,20 +9,9 @@ namespace CSharp
 {
     class Program
     {
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
-            string file_name = @"C:\Users\Spark\Desktop\c-sharp\CSharp\CSharp\text.txt";
-            string text = "Hello man";
-
-            StreamWriter sw = File.CreateText(file_name);
-            sw.WriteLine(text);
-            sw.WriteLine("How are you ?");
-            sw.Close();
-
-            StreamReader sr = File.OpenText(file_name);
-            Console.WriteLine(sr.ReadToEnd());
-            sr.Close();
-
+            Users user1 = new Users("Дима", 25, "Администатор");
             Console.ReadKey();
         }
     }
---
+++ CSharp/CSharp/Users.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Users
+    {
+        private string name;
+        private int age;
+        public Admin admin = new Admin();
+
+        public Users(string name, int age, string role) {
+            this.name = name;
+            this.age = age;
+            admin.role = role;
+            Console.WriteLine($"Меня зовут {name}, Мне {age} лет и я {role}");
+        }
+
+        public void setUser(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void printAll()
+        {
+            Console.WriteLine($"Имя: {name}, Возраст {age}");
+        }
+
+        public void setAdmin(string role)
+        {
+            admin.role = role;
+        }
+
+
+    }
+}
