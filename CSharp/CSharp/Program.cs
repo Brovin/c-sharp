ec12f1144bafcd40d537ffbd38b5815bb976ec97
Boris <fody@protonmail.com>
2019-05-02 22:38:12 +0300

    update

--- CSharp/CSharp/Program.cs
+++ CSharp/CSharp/Program.cs
@@ -9,33 +9,9 @@ namespace CSharp
 {
     class Program
     {
-         delegate void Message();
-         delegate int Operation(int x, int y);
-
          static void Main(string[] args)
         {
-            Message mes;
 
-            if(DateTime.Now.Hour < 12){
-                mes = GoodMorning;
-            } else {
-                mes = GoodEvening;
-            }
-
-            mes();
-
-            Operation operation = (x, y) => x * y;
-            Console.WriteLine(operation(5,5));
-
-            Console.ReadKey();
-        }
-
-        private static void GoodMorning(){
-            Console.WriteLine("Good Morning");    
-        }
-
-        private static void GoodEvening() {
-            Console.WriteLine("Good Evening");
         }
     }
 }
